4dfb508c4b20939bd374e3552f44dcffe0056a29
zajitschek <64367116+zajitschek@users.noreply.github.com>
2020-06-24 12:32:40 +0100

    install.R and runtime.txt

---
+++ install.R
@@ -0,0 +1,7 @@
+install.packages("magrittr")
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("rjson")
+install.packages("metafor")
+install.packages("compute.es")
+install.packages("ape")
