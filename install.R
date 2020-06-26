7b805846e4e71b431b5d2efc899254b9680381b4
zajitschek <64367116+zajitschek@users.noreply.github.com>
2020-06-26 23:05:18 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,7 +1,6 @@
 install.packages("magrittr")
 install.packages("dplyr")
 install.packages("ggplot2")
-install.packages("rjson")
+
 install.packages("metafor")
 install.packages("compute.es")
-install.packages("ape")
